88061deb9eab4270b28abc6f09edb5ebf8457f31
Sviatoslav Khmelevskyi <lechstanislaus@gmail.com>
2021-09-27 19:23:23 +0300

    Create function for checking EM algorithm performance

--- func_DigitRecognizer.py
+++ func_DigitRecognizer.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+
 def LearningAlg(df, iter=10, eps=5e-2, eta=1e-1):
     """Implements EM-algorithm for clustering two classes of images.
       >>> np.max(LearningAlg(pd.read_csv( "data/mini_test.csv" ))[0], axis = 0)
@@ -64,6 +65,59 @@ def LearningAlg(df, iter=10, eps=5e-2, eta=1e-1):
         p_k_x[0] = nu * p_k[0] / (p_k[1] + nu * p_k[0])
         p_k_x[1] = p_k[1] / (p_k[1] + nu * p_k[0])
     return P, p_k
+
+
+def CheckAlg(df, P, p_k):
+    """Checks performance of EM-algorithm on dataset.
+       >>> CheckAlg(pd.read_csv( "data/mini_test.csv" ), np.array([np.zeros((4,4))+0.95,np.zeros((4,4))+0.05]), np.array([0.5, 0.5]))
+       1.0
+       >>> CheckAlg(0, np.array([np.zeros((4,4))+0.95,np.zeros((4,4))+0.05]), np.array([0.5, 0.5]))
+       Traceback (most recent call last):
+        ...
+       TypeError: df must be pandas dataframe
+       >>> CheckAlg(pd.read_csv( "data/mini_test.csv" ), 0, np.array([0.5, 0.5]))
+       Traceback (most recent call last):
+        ...
+       TypeError: P and p_k must be numpy array
+       >>> CheckAlg(pd.read_csv( "data/mini_test.csv" ), np.array([np.zeros((4,4))+0.95,np.zeros((4,4))+0.05]), np.array(['a', 0.5]))
+       Traceback (most recent call last):
+        ...
+       TypeError: wrong type of elements of P or p_k
+       >>> CheckAlg(pd.read_csv( "data/mini_test.csv" ), np.array([np.zeros((4,4))+0.95,np.zeros((4,4))+0.05]), np.array([0.5, 0.5, 0.5]))
+       Traceback (most recent call last):
+        ...
+       ValueError: P must have two sub-matrices and p_k must have two values
+       >>> CheckAlg(pd.read_csv( "data/mini_test.csv" ), np.array([np.zeros((4,4))+0.95,np.zeros((4,4))+0.05]), np.array([2., 0.5]))
+       Traceback (most recent call last):
+        ...
+       ValueError: elements of P and p_k must be > 0 and < 1
+       >>> CheckAlg(pd.read_csv( "data/mini_test.csv" ), np.array([np.zeros((5,5))+0.95,np.zeros((5,5))+0.05]), np.array([0.5, 0.5]))
+       Traceback (most recent call last):
+        ...
+       ValueError: P must have the same shape as data
+    """
+    if not isinstance( df, pd.DataFrame ):
+        raise TypeError( "df must be pandas dataframe" )
+    if type( P ) != np.ndarray or type( p_k ) != np.ndarray:
+        raise TypeError( "P and p_k must be numpy array" )
+    if P.dtype != 'float64' or p_k.dtype != 'float64':
+        raise TypeError( "wrong type of elements of P or p_k" )
+    if P.shape[0] != 2 or p_k.shape[0] != 2:
+        raise ValueError( "P must have two sub-matrices and p_k must have two values" )
+    if not (np.prod(0. < P) * np.prod(P < 1.) and np.prod(0. < p_k) * np.prod(p_k < 1.)):
+        raise ValueError( "elements of P and p_k must be > 0 and < 1" )
+    num = int(df.columns[len( df.columns ) - 1].split( sep='x' )[0])
+    if P.shape[1] != num or P.shape[2] != num:
+        raise ValueError( "P must have the same shape as data" )
+    X = df.to_numpy()[:, 1:].T.reshape( (num, num, len( df )) )
+    p_x_k = np.zeros( (2, len( df )) )
+    p_x_k[0] = np.prod( np.prod( (P[0] ** X.T) * ((1 - P[0]) ** (1 - X.T)), axis=1 ), axis=1 )
+    p_x_k[1] = np.prod( np.prod( (P[1] ** X.T) * ((1 - P[1]) ** (1 - X.T)), axis=1 ), axis=1 )
+    Pr = (p_x_k.T * p_k).T / (np.sum( p_x_k.T * p_k, axis=1 ) + np.array( [np.sum( p_x_k.T * p_k, axis=1 ) == 0] ))
+    Guess = np.array( [0 if Pr[0][i] > Pr[1][i] else 1 for i in range( len( df ) )] )
+    Q = np.sum( np.array( [Guess == df.label] )[0] ) / len( df )
+    return max( Q, 1 - Q )
+
 
 if __name__ == "__main__":
     import doctest
--- main_DigitRecognizer.py
+++ main_DigitRecognizer.py
@@ -2,6 +2,10 @@ import pandas as pd
 import func_DigitRecognizer as dr
 
 train_df = pd.read_csv( "data/short_mnist_train_99.csv" )
+test_df = pd.read_csv( "data/short_mnist_test_99.csv" )
 
 P_t, p_t = dr.LearningAlg( train_df, iter=50, eps=7e-2, eta=1e-1 )
-print(P_t, p_t, sep = '\n')
+
+train_res = dr.CheckAlg(train_df, P_t, p_t)
+test_res = dr.CheckAlg(test_df, P_t, p_t)
+print('Train res: '+str(train_res),'Test res: '+str(test_res), sep = '\n')
